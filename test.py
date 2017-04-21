cb324bec6d434774875af32bf1c4f1b2f43e36ec
Shubham Sawant <shubham.sawant@walchandsangli.ac.in>
2017-04-21 14:55:49 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,11 +3,11 @@ from upload.uploader import *
 
 
 def test_cchef():
-	user = cchef('moghya','helpit70.')
+	user = cchef('cchefUsername','cchefPassword.')
 	user.getSubmissions()
 
 def test_upToGithub():
-	user = upToGithub('moghya','helpit70.')
-	user.cchefUpload('moghya','helpit70.')
+	user = upToGithub('githubUsername','githubPassword')
+	user.cchefUpload('cchefUsername','cchefPassword.')
 
 test_upToGithub()
